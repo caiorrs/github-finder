a52369f56a725c22e09166acd3ac33e575ac6910
Caio Roberto <caiorrs@gmail.com>
2022-02-06 13:05:17 -0300

    Getting users from github api

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,32 @@
+import { API_TOKEN, API_URL } from "../../environment";
+import { useEffect, useState } from "react";
+
+const UserResults = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    setLoading(true);
+    const response = await fetch(`${API_URL}/users`, { headers: { Authorization: `Bearer ${API_TOKEN}` } });
+    const data = await response.json();
+
+    setUsers(data);
+    setLoading(false);
+  };
+
+  return loading ? (
+    <h3>Loading...</h3>
+  ) : (
+    <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+      {users.map((user) => (
+        <h3 key={user.login}>{user.login}</h3>
+      ))}
+    </div>
+  );
+};
+
+export default UserResults;
---
+++ src/environment.js
@@ -0,0 +1,4 @@
+module.exports = {
+  API_URL: process.env.REACT_APP_GITHUB_URL,
+  API_TOKEN: process.env.REACT_APP_API_TOKEN,
+};
